1306fc987c3d33d2fa305fdcd39ab8939503e6ce
ENNOUARI FATIMA-EZZAHRA <fatimaezzahraennouari@gmail.com>
2025-06-26 15:14:00 +0100

    feat(etudiant): permettre la création de compte étudiant

--- backend/src/main/java/com/taliibHub/backend/dto/RegisterRequest.java
+++ backend/src/main/java/com/taliibHub/backend/dto/RegisterRequest.java
@@ -2,14 +2,13 @@ package com.taliibHub.backend.dto;
 
 public class RegisterRequest {
     private String nom;
-    private String prenom;
     private String email;
     private String motDePass;
-    private com.taliibHub.backend.enums.RoleUtilisateur role;
 
     public String getNom() { return nom; }
-    public String getPrenom() { return prenom; }
+    public void setNom(String nom) { this.nom = nom; }
     public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
     public String getMotDePass() { return motDePass; }
-    public com.taliibHub.backend.enums.RoleUtilisateur getRole() { return role; }
+    public void setMotDePass(String motDePass) { this.motDePass = motDePass; }
 }
